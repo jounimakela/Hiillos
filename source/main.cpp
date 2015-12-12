0b14495331ce3325e9cdf722ba4bdad73ca9a486
pabn1 <root@jounimakela.com>
2015-12-12 03:54:29 +0200

    Testing in main

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,37 @@
-#include <stdio.h>
+#include <iostream>
 
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_ttf.h"
-#include "SDL2/SDL_mixer.h"
+#include "SFML/Window.hpp"
 
-int main(int argc, char* args[]) {
+#include "vendor/easylogging++.h"
+#include "AssetManager.h"
+#include "SceneDirector.h"
+#include "SceneFactory.h"
+#include "Scene.h"
 
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Quit();
+INITIALIZE_EASYLOGGINGPP
 
-	return 0;
+int main(int argc, char* args[])
+{
+        /* Logger configuration */
+        el::Configurations conf("./config/logger.conf");
+        el::Loggers::reconfigureAllLoggers(conf);
+        LOG(INFO) << "Starting application.";
+
+        sf::RenderWindow window(sf::VideoMode(800, 600), "Hiillos - development");
+
+        SceneFactory::Get()->Register("GameScene", &GameScene::Create);
+        SceneDirector director(&window);
+        director.push("GameScene");
+
+        director.run();
+
+        /*
+        ImageAsset texture = ImageManager.fetch("asset.png");
+
+        sf::Sprite sprite;
+        sprite.setTexture(texture.data());
+        sprite.setPosition(0, 0);
+        */
+
+        return 0;
 }
